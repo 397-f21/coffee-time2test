dcb89e145d27dda164a54939449fa8c7a6a8a1c4
jakerogers2022 <jakerogers2022@u.northwestern.edu>
2021-10-12 21:04:24 -0500

    initial modal working

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "react-datepicker/dist/react-datepicker.css";
 import "./styles.css";
 import React, { useState } from "react";
 import Typography from '@mui/material/Typography';
-
+import Modal from 'react-modal';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
 
 import DateTimePicker from '@mui/lab/DateTimePicker';
@@ -31,6 +31,7 @@ const renderTime = (dimension, time) => {
 };
 
 
+
 const getTimeMinutes = (time) => ((time % hourSeconds) / minuteSeconds) | 0;
 const getTimeHours = (time) => ((time % daySeconds) / hourSeconds) | 0;
 const getTimeDays = (time) => (time / daySeconds) | 0;
@@ -44,11 +45,44 @@ export default function App() {
   var daysDuration = days * daySeconds;
   const [curKey, setCurKey] = React.useState(endTime)
 
-
-  const handleChange = (newEndTime) => {
+  const handleDateChange = (newEndTime) => {
     setEndTime(newEndTime);
     setCurKey(newEndTime);
   };
+
+  const [examList, setExamList] = useState([])
+  const [AddingExamName, setAddExamName] = useState('')
+
+  let subtitle;
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = '#f00';
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function addExam() {
+    setIsOpen(false);
+    console.log("original: " + examList)
+    let newExamList = [...examList,{'name': AddingExamName, 'date': endTime}]
+    setExamList(newExamList);
+    console.log("new: " + newExamList)
+    console.log("examList " + examList)
+    setAddExamName('')
+  }
+
+  const handleChangeExamName = (x) => {
+    setAddExamName(x.currentTarget.value)
+  }
+
 
   return (
     <div className="App">
@@ -60,6 +94,31 @@ export default function App() {
           MCAT
         </Typography>
       </div>
+      <button onClick={openModal}>Open Modal</button>
+      
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        ariaHideApp={false}
+        contentLabel="Example Modal"
+      >        <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Enter Exam Information</h2>
+      <div>Exam Name:</div>
+      <form>
+        <input type="text" value={AddingExamName} onChange={(x) => handleChangeExamName(x)} />
+      </form>
+      <div className={"Local"}>
+        <LocalizationProvider dateAdapter={AdapterDateFns} >
+          <DateTimePicker
+              value={endTime}
+              onChange={handleDateChange}
+              renderInput={(params) => <TextField {...params} />}
+          />
+        </LocalizationProvider>
+      </div>
+      <button onClick={closeModal}>close</button>
+      <button onClick={addExam}>Add</button>
+    </Modal>
       <div className="circles">
         <CountdownCircleTimer
           {...timerProps}
@@ -100,15 +159,6 @@ export default function App() {
             renderTime("minutes", getTimeMinutes(hourSeconds - elapsedTime))
           }
         </CountdownCircleTimer>
-      </div>
-      <div className={"Local"}>
-        <LocalizationProvider dateAdapter={AdapterDateFns} >
-          <DateTimePicker
-              value={endTime}
-              onChange={handleChange}
-              renderInput={(params) => <TextField {...params} />}
-          />
-        </LocalizationProvider>
       </div>
     </div>
   );
